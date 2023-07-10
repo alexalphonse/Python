c40cab12c60e804818c2039399c42d5b6a31e71f
Alex Alphonse <alexalphonse.developer@gmail.com>
2023-07-11 00:31:49 +0530

    Day 01

---
+++ Band_name_Generator.py
@@ -0,0 +1,14 @@
+# Create a Welcome Note
+print("Welcome to the Band Name Generator")
+
+# Ask which city they grew up in
+
+City = input("Which city did you grow up in?\n")
+
+# Ask what is thier pets name
+
+Pet_name = input("What is your pet' name?\n")
+
+#Result
+
+print("Your BAND NAME is " + City + " " + Pet_name )
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Day 01
+    Learnt How to Create Hello World in python.
+    understood what print, input and variable function does
